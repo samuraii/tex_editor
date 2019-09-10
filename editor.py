5fe4967052e9abd2193bd792aa12f2e3ba6b2f64
samuraii <7thsamurai@mail.ru>
2019-09-10 16:30:07 +0300

    Добавление меню в приложение

--- editor.py
+++ editor.py
@@ -2,4 +2,16 @@ from tkinter import *
 
 root = Tk()
 
+# Top menu bar creating
+menu_bar = Menu(root)
+file_menu = Menu(menu_bar, tearoff=0)
+edit_menu = Menu(menu_bar, tearoff=0)
+view_menu = Menu(menu_bar, tearoff=0)
+about_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='File', menu=file_menu)
+menu_bar.add_cascade(label='Edit', menu=edit_menu)
+menu_bar.add_cascade(label='View', menu=view_menu)
+menu_bar.add_cascade(label='About', menu=about_menu)
+root.config(menu=menu_bar)
+
 root.mainloop()
