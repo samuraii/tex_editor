b259ccfb6e158f3f79dc9ab1b21ad051fcbd22a0
samuraii <7thsamurai@mail.ru>
2019-09-11 13:47:33 +0300

    Завершение структуры меню

--- editor.py
+++ editor.py
@@ -79,6 +79,44 @@ def showhelp():
 about_menu.add_command(label="Help", compound='left', image=None, underline=0, command=showhelp)
 about_menu.add_command(label="About", compound='left', image=None, underline=0, command=about)
 
+# View menu
+show_line_no = BooleanVar()
+highlight_line = BooleanVar()
+show_whitespaces = BooleanVar()
+view_menu.add_checkbutton(label="Show Line Number", variable=show_line_no)
+view_menu.add_checkbutton(label="Highlight current line", variable=highlight_line)
+view_menu.add_checkbutton(label="Show whitespaces", variable=show_whitespaces)
+
+color_schemes = {
+    'Default': '#000000.#FFFFFF',
+    'Greygarious': '#83406A.#D1D4D1',
+    'Aquamarine': '#5B8340.#D1E7E0',
+    'Bold Beige': '#4B4620.#FFF0E1',
+    'Cobalt Blue': '#ffffBB.#3333aa',
+    'Olive Green': '#D1E7E0.#5B8340',
+    'Night Mode': '#FFFFFF.#000000',
+}
+
+themes_menu = Menu(menu_bar, tearoff=0)
+view_menu.add_cascade(label='Themes', menu=themes_menu)
+theme_choice = StringVar()
+theme_choice.set('Default')
+for k in sorted(color_schemes):
+    themes_menu.add_radiobutton(label=k, variable=theme_choice)
+
+# Line numbers and shortcuts menu
+shortcut_bar = Frame(root, height=25, background='seagreen')
+shortcut_bar.pack(expand='no', fill='x')
+line_number_bar = Text(root, width=4, padx=2, takefocus=0, border=0, background='khaki', state='disabled', wrap='none')
+line_number_bar.pack(side='left', fill='y')
+
+# Textframe
+content_text = Text(root, wrap='word')
+content_text.pack(expand='yes', fill='both')
+scroll_bar = Scrollbar(content_text)
+content_text.configure(yscrollcommand=scroll_bar.set)
+scroll_bar.config(command=content_text.yview)
+scroll_bar.pack(side='right', fill='y')
 
 root.config(menu=menu_bar)
 
