074a46aaa336c9c1f855f98fe3f81dbea75f2d3f
samuraii <7thsamurai@mail.ru>
2019-09-11 08:47:03 +0300

    Продолжение работы с меню

--- editor.py
+++ editor.py
@@ -7,12 +7,10 @@ root.title(PROGRAM_NAME)
 
 # Top menu bar creating
 menu_bar = Menu(root)
-
 file_menu = Menu(menu_bar, tearoff=0)
 edit_menu = Menu(menu_bar, tearoff=0)
 view_menu = Menu(menu_bar, tearoff=0)
 about_menu = Menu(menu_bar, tearoff=0)
-
 menu_bar.add_cascade(label='File', menu=file_menu)
 menu_bar.add_cascade(label='Edit', menu=edit_menu)
 menu_bar.add_cascade(label='View', menu=view_menu)
@@ -41,7 +39,46 @@ file_menu.add_command(label="Exit", accelerator='Ctrl+Q', compound='left', image
 def undo_callback():
     pass
 
+def redo_callback():
+    pass
+
+def cut_callback():
+    pass
+
+def copy_callback():
+    pass
+
+def paste_callback():
+    pass
+
+def find_callback():
+    pass
+
+def select_all_callback():
+    pass
+
+# Edit menu items
 edit_menu.add_command(label="Undo", accelerator='Ctrl + Z', compound='left', image=None, underline=0, command=undo_callback)
+edit_menu.add_command(label="Redo", accelerator='Ctrl + Y', compound='left', image=None, underline=0, command=redo_callback)
+edit_menu.add_separator()
+edit_menu.add_command(label="Cut", accelerator='Ctrl + X', compound='left', image=None, underline=0, command=cut_callback)
+edit_menu.add_command(label="Copy", accelerator='Ctrl + C', compound='left', image=None, underline=0, command=copy_callback)
+edit_menu.add_command(label="Paste", accelerator='Ctrl + V', compound='left', image=None, underline=0, command=paste_callback)
+edit_menu.add_separator()
+edit_menu.add_command(label="Find", accelerator='Ctrl + F', compound='left', image=None, underline=0, command=find_callback)
+edit_menu.add_separator()
+edit_menu.add_command(label="Select All", accelerator='Ctrl + A', compound='left', image=None, underline=0, command=select_all_callback)
+
+def about():
+    pass
+
+def showhelp():
+    pass
+
+# Help menu items
+about_menu.add_command(label="Help", compound='left', image=None, underline=0, command=showhelp)
+about_menu.add_command(label="About", compound='left', image=None, underline=0, command=about)
+
 
 root.config(menu=menu_bar)
 
