2f455c5759e4b5e07ca4cb955d26820a8381a6e5
samuraii <7thsamurai@mail.ru>
2019-09-11 08:37:32 +0300

    Продолжение работы с меню

--- editor.py
+++ editor.py
@@ -1,17 +1,48 @@
 from tkinter import *
 
+PROGRAM_NAME = "text editor"
+
 root = Tk()
+root.title(PROGRAM_NAME)
 
 # Top menu bar creating
 menu_bar = Menu(root)
+
 file_menu = Menu(menu_bar, tearoff=0)
 edit_menu = Menu(menu_bar, tearoff=0)
 view_menu = Menu(menu_bar, tearoff=0)
 about_menu = Menu(menu_bar, tearoff=0)
+
 menu_bar.add_cascade(label='File', menu=file_menu)
 menu_bar.add_cascade(label='Edit', menu=edit_menu)
 menu_bar.add_cascade(label='View', menu=view_menu)
 menu_bar.add_cascade(label='About', menu=about_menu)
+
+def new_file():
+    pass
+
+def open_file():
+    pass
+
+def save_file():
+    pass
+
+def save_as_file():
+    pass
+
+# File menu items
+file_menu.add_command(label="New", accelerator='Ctrl+N', compound='left', image=None, underline=0, command=new_file)
+file_menu.add_command(label="Open", accelerator='Ctrl+O', compound='left', image=None, underline=0, command=open_file)
+file_menu.add_command(label="Save", accelerator='Ctrl+S', compound='left', image=None, underline=0, command=save_file)
+file_menu.add_command(label="Save As..", accelerator='Ctrl+Shift+S', compound='left', image=None, underline=0, command=save_as_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", accelerator='Ctrl+Q', compound='left', image=None, underline=0, command=root.destroy)
+
+def undo_callback():
+    pass
+
+edit_menu.add_command(label="Undo", accelerator='Ctrl + Z', compound='left', image=None, underline=0, command=undo_callback)
+
 root.config(menu=menu_bar)
 
 root.mainloop()
